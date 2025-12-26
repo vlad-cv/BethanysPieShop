04b32216204b7c1b66eb3833f6b9f68e58a26ad7
vlad-cv <me@vladcv.dev>
2025-12-26 03:56:51 -0600

    Set current category in Pie list; update HTML structure
    PieController's List action now sets ViewBag.CurrentCategory to "Cheese cakes" for use in the view. Updated _Layout.cshtml with charset and improved viewport meta tags. Replaced List.cshtml with a full HTML5 structure that displays the current category and sets the page title.

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -17,6 +17,7 @@ namespace BethanysPieShop.Controllers
         }
         public IActionResult List()
         {
+            ViewBag.CurrentCategory = "Cheese cakes";
             return View(_pieRepository.AllPies);
         }
     }
